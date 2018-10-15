fcfa4e446a0c3a844f42f55480e000ae0e963000
Hiren <hiren@freshbits.in>
2018-10-15 18:26:54 +0530

    Remove extra JS CODES

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,6 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
