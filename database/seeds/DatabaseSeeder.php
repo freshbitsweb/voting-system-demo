e8c24bb5d16d59e91a3d6a6c3581f89fb75668dc
Hiren <hiren@freshbits.in>
2018-10-16 18:12:47 +0530

    seed static topics

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        foreach ($this->getStaticTopics() as $topic) {
+            factory(App\Topic::class)->create([
+                'title' => $topic
+            ]);
+        }
         factory(App\User::class, 10)->create();
+    }
+
+    /**
+     * Returns the list of topics
+     *
+     * @return array
+     **/
+    public function getStaticTopics()
+    {
+        return [
+            'Package Development',
+            'Performant Laravel',
+            'In-depth Authentication',
+            'Headache-free Deployments',
+            'API development',
+            'Queues & Jobs',
+            'Testing in Laravel',
+            'Advanced Eloquent',
+            'Vue.JS',
+        ];
     }
 }
