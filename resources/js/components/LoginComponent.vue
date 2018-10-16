2582715ca7b53a7616d36f0f5c2c0b16154210b6
Hiren <hiren@freshbits.in>
2018-10-16 11:09:59 +0530

    remove remember checkbox

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -40,21 +40,6 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input type="checkbox"
-                                            class="form-check-input"
-                                            name="remember"
-                                            id="remember"
-                                        >
-
-                                        <label class="form-check-label" for="remember">
-                                            Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
